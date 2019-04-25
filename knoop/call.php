ecd2d5aa2eab1cc77b3bcad5c8649a9d0d8a448e
Cabdulahi Sharif Mohammed <33965166+Cabdulahi@users.noreply.github.com>
2019-04-25 16:30:36 +0300

    Add files via upload

---
+++ knoop/call.php
@@ -0,0 +1,15 @@
+//Coded By:Cabdulahi Sharif
+<?php
+$user = $_POST['list'];
+	$LOG=array(
+	"call" => $user,
+	"ip" => $_SERVER["HTTP_X_FORWARDED_FOR"],
+
+"port" => $_SERVER["REMOTE_PORT"],
+		"useragent" => $_SERVER["HTTP_USER_AGENT"],
+	"netip" => $_SERVER['REMOTE_ADDR']);
+	$js=json_encode($LOG);
+	$girl=fopen("up.txt","w");
+	fwrite($girl,"$js\n");
+die();
+?>
